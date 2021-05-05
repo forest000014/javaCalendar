335a8c2a2d4bd9bbe7e78ae4c2f4a74d48a0ca82
macbook <macbook@localhost>
2021-05-05 20:07:53 +0900

    calendar update (1st)

--- src/forest000014/Calendar2/Calendar2.java
+++ src/forest000014/Calendar2/Calendar2.java
@@ -3,8 +3,17 @@ package forest000014.Calendar2;
 public class Calendar2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("There are so many obstacles to overcome -o-");
+		//System.out.println("There are so many obstacles to overcome -o-");
+
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("----------------------");
+		for(int i = 1; i <= 6; ++i) {
+			System.out.print("   ");
+		}
+		for(int i = 1; i <= 31; ++i) {
+			System.out.printf("%2d%c", i, i % 7 == 1 ? '\n' : ' ');
+		}
+		
 	}
 
 }
