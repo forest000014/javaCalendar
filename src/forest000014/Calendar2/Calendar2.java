ad83162d12096ccf894bc750ccdd724f9f115676
macbook <macbook@localhost>
2021-05-05 19:54:45 +0900

    Init project

---
+++ src/forest000014/Calendar2/Calendar2.java
@@ -0,0 +1,10 @@
+package forest000014.Calendar2;
+
+public class Calendar2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("There are so many obstacles to overcome -o-");
+	}
+
+}
